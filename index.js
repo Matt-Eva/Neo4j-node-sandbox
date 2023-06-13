415e1c3d7c67e75a4288fe39d601382a5ffb2016
Matt Eva <matteweva@gmail.com>
2023-06-13 09:19:53 -0700

    saving changes

--- index.js
+++ index.js
@@ -5,11 +5,47 @@ const neo = require('neo4j-driver')
 const app = express()
 const port = 3000
 const uri = process.env.NEO_URL
+const user = process.env.NEO_USER
+const password = process.env.NEO_PASSWORD
+let driver;
 
-app.listen(port, () =>{
+const server = app.listen(port, () =>{
     console.log(`app running on ${port}`)
 })
 
-async function connectNeo(){
+server.on('listening', async () =>{
+    console.log("server listening")
+    await connectNeo()
+    console.log("resources ready")
+    runQuery()
+})
 
+async function runQuery(){
+    try{
+        const query = 'SHOW DATABASES'
+        const result = await driver.executeQuery(query, {})
+        // console.log(records.fields)
+        // console.log(summary)
+        console.log(result)
+    } catch(err){
+        console.error(err)
+    }
+}
+
+async function connectNeo(){
+    try{
+        driver = neo.driver(uri, neo.auth.basic(user, password))
+        await driver.verifyConnectivity()
+        console.log("connection established")
+    } catch(err){
+        console.error(error)
+        closeServer()
+    }   
+}
+
+function closeServer(){
+    server.close(() =>{
+        console.log("server closing")
+        process.exit(0)
+    })
 }
